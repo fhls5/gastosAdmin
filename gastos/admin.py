3b3a2cc75ecac06b55de0d75c79425833c4bd6a0
lopezllanes <malolla.lopez@gmail.com>
2019-05-12 12:19:32 -0400

    commit categoria

--- gastos/admin.py
+++ gastos/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Gastos
+from .models import Categorias
 
-admin.site.register(Gastos)
+#admin.site.register(Gastos)
+class GastosAdmin(admin.ModelAdmin):
+    fields = ['monto', 'categoria']
+
+admin.site.register(Gastos, GastosAdmin)
+
+class CategoriasAdmin(admin.ModelAdmin):
+    fields = ['nombre']
+
+admin.site.register(Categorias, CategoriasAdmin)
